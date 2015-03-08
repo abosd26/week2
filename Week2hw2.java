5467d799b7748ae96aa0963628ac32549a441354
abosd26 <d199469@yahoo.com.tw>
2015-03-08 22:54:17 +0800

    week2 homework2

---
+++ Week2hw2.java
@@ -0,0 +1,16 @@
+class Week2hw2
+{
+	public static void main(String[] args)
+	{
+		int[] num = {1, 2, 3, 4, 5, 6, 7};
+		int sum = 0;
+		double average;
+		for(int i : num)
+		{
+			sum += i;
+		}
+		average = sum / num.length;
+		System.out.println("Á`©M : " + sum);
+		System.out.println("¥­§¡ : " + average);
+	}
+}
