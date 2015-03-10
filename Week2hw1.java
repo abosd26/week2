4e31d32e2eaf1fb1718c86fdf3edfb7a7f98a86d
abosd26 <d199469@yahoo.com.tw>
2015-03-10 23:02:09 +0800

    week2 homework1

---
+++ Week2hw1.java
@@ -0,0 +1,9 @@
+class Week2hw1
+{
+	public static void main(String[] args)
+	{
+		int t1 = 4 * 60 * 60 + 23 * 60 + 45;
+		int t2 = 13 * 60 * 60 + 54 * 60 + 23;
+		System.out.println("Á`¬í¼Æ¿é¥X : " + (t2 - t1) + "¬í");
+	}
+}
